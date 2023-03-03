a54946461dcc56e0bc8b65b012d124569c2fff35
FronMakerKim <wkadht0619@gmail.com>
2023-03-03 23:25:44 +0900

    js거의 완성

--- main.js
+++ main.js
@@ -1,7 +1,72 @@
-const $td = document.querySelector("td");
+const $display = document.querySelector(".display");
+const $result = document.querySelector(".result");
 
-$td.onclick = hello();
+//display
+function add(value) {
+  const p = $display.textContent[$display.textContent.length - 1];
+  if ($result.textContent === "0") {
+    $result.textContent = value;
+  } else {
+    $result.textContent += value;
+  }
+  if (
+    (p === "+" || p === "-" || p === "/" || p === "x") &&
+    $display.textContent.slice(0, $display.textContent.length - 2) ===
+      $result.textContent.slice(0, $result.textContent.length - 2)
+  ) {
+    $result.textContent = "";
+    if ($result.textContent === "") $result.textContent = value;
+  }
+}
 
-function hello() {
-  console.log("htllo");
+//연산기호
+function calc(value) {
+  const displayValue = $display.textContent;
+  $display.textContent = $result.textContent;
+  switch (value) {
+    case "+":
+      if (displayValue[displayValue.length - 1] === "+") {
+        const result =
+          Number($result.textContent) + Number(displayValue.split("+")[0]);
+        $result.textContent = result;
+        $display.textContent = result;
+      }
+      $display.textContent += "+";
+      break;
+    case "-":
+      $display.textContent += "-";
+      break;
+    case "x":
+      $display.textContent += "*";
+      break;
+    case "/":
+      $display.textContent += "/";
+      break;
+  }
+}
+
+//=
+function equal() {
+  $display.textContent += $result.textContent;
+  $result.textContent = eval($display.textContent);
+}
+
+//<
+function back() {
+  let arr = $result.textContent.split("");
+  arr.pop();
+  $result.textContent = arr.join("");
+  if ($result.textContent.length === 0) {
+    $result.textContent = 0;
+  }
+}
+
+//CE
+function CE() {
+  $result.textContent = 0;
+}
+
+//소수점
+function point() {
+  $display.textContent += ".";
 }
